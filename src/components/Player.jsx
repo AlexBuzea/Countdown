bee30fcd2eab598a3ac6381a7a8775f319c0d0b8
Alex Buzea <alexbuzea@ymail.com>
2025-04-28 14:24:44 +0100

    saving user name with ref instead of useState to prevent re-evaluating the component on each keystroke and prevent changing the player name after submitting. Instead will only update the name after submit button clicked.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,32 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  // const [submitted, setSubmitted] = useState(false);
+  // function handleChange(e) {
+  //   setSubmitted(false);
+  //   setEnteredPlayerName(e.target.value);
+  // }
+
+  function handleClick() {
+    // setSubmitted(true);
+    console.log(playerName);
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = "";
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      {/* <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"} </h2> */}
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          ref={playerName}
+          type="text"
+          // onChange={handleChange}
+          // value={enteredPlayerName}
+        />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
